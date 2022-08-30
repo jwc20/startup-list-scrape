cf8ad475e729ab4ed2c8bcb8c0958d42fb8ba28f
jwc20 <choij2015@gmail.com>
2022-08-30 16:20:31 -0700

    created crawler boiler

---
+++ startuppill/cb_crawler.py
@@ -0,0 +1,29 @@
+
+import requests
+from bs4 import BeautifulSoup
+from collections import namedtuple
+import csv
+
+
+class CBCrawler:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def _load_page():
+        pass
+
+    @staticmethod
+    def _scrape_page():
+        pass
+
+    def get_rank(self):
+        pass
+
+    def export_to_csv(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
+    # CBCrawler().get_rank();
